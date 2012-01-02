9cf6fbf0eaa0a763abaeb23bd0df64adccc4966d
Justin Patera <serialhex@gmail.com>
2012-01-01 21:18:34 -0500

    done first bit of parsing, now return values

---
+++ ch02/parsing.hs
@@ -0,0 +1,18 @@
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value! YAY!!!"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn (readExpr (args !! 0))
